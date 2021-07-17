72df9e545c5fcfbf434b79ce4c119db48a777a95
Quoc Anh Truong <teomoney1999@gmail.com>
2021-07-17 15:26:42 +0700

    init

--- run.py
+++ run.py
@@ -7,13 +7,13 @@ app.config.FORWARDED_SECRET = 'acndef11371ndjacn'
 @app.get("/") 
 def index(request): 
     return text(
-        f"{request.remote_addr} connected to {request.url_for("index")} \n"
+        f"{request.remote_addr} connected to {request.url_for('index')} \n"
         f"Forwarded: {request.forwarded}\n"
     )
 
 @app.get('/test')
 def test(request): 
     return text("Hello, World")
-    
+
 if __name__ == '__main__': 
-    app.run(host='127.0.0.1', port=8000, worker=8, access_log=False)
+    app.run(host='127.0.0.1', port=8000, workers=8, access_log=False)
